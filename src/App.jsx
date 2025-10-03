339ac80bb52cfaf16da26c3fdbbbb66f81d7b9fd
mabuzaS21 <mabuzasfiso05@gmail.com>
2025-10-03 15:46:45 +0200

    Initial commit:Setup React project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold mb-6">Weather Dashboard</h1> 
+    </div>
+  );
+}
+
+export default App;
