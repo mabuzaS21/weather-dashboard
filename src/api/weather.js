232ab4ac7f31a0f0881d08b3d0dfd4783abae18d
mabuzaS21 <mabuzasfiso05@gmail.com>
2025-10-04 23:37:56 +0200

    Add API folder with weather data fetching logic

---
+++ src/api/weather.js
@@ -0,0 +1,18 @@
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+export async function fetchWeather(city) {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+    );
+
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
